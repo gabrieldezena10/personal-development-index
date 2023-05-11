43732caacde21767e10c2ff8102636bd5fae3f86
Gabriel Dezena Torres <gabriel.deztor@gmail.com>
2023-05-11 20:31:22 +0000

    feat: implement App screen

--- projects/scale-react-dev-with-nx/store-application/src/app/app.tsx
+++ projects/scale-react-dev-with-nx/store-application/src/app/app.tsx
@@ -1,16 +1,49 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
 import styles from './app.module.scss';
-
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { getAllGames } from './fake-api';
 
 
 export function App() {
   return (
-    <h1>Hi test</h1>
+    <>
+      <div className="container">
+        <div className="games-layout">
+          {getAllGames().map((x) => (
+            <Card key={x.id} className="game-card">
+              <CardActionArea>
+                <CardMedia
+                  className="game-card-media"
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className="game-rating"
+                  >
+                    <strong>Rating:</strong> {x.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
