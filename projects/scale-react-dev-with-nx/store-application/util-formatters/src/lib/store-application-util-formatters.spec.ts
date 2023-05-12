876f8781eee36780a7030d43fef3f9c1102ef231
Gabriel Dezena Torres <gabriel.deztor@gmail.com>
2023-05-12 12:29:49 +0000

    feat: create util-formatters js-library

---
+++ projects/scale-react-dev-with-nx/store-application/util-formatters/src/lib/store-application-util-formatters.spec.ts
@@ -0,0 +1,9 @@
+import { storeApplicationUtilFormatters } from './store-application-util-formatters';
+
+describe('storeApplicationUtilFormatters', () => {
+  it('should work', () => {
+    expect(storeApplicationUtilFormatters()).toEqual(
+      'store-application-util-formatters'
+    );
+  });
+});
