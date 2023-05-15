22f52ae94dbe99c467c888ba5b08017867ed80d2
gabriel.torres <gabriel.torres@dynamox.net>
2023-05-15 16:31:40 -0300

    feat: implement custom command and subscribe.cy.ts

---
+++ projects/cypress-realworld-testing-course-app-start/cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,27 @@
+import { contains } from "cypress/types/jquery"
+
+describe("Newsletter Subscribe Form", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000")
+    })
+  
+    it("allows users to subscribe to the email list", () => {
+        cy.getByData("email-input").type("tom@aol.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains("tom@aol.com")
+
+    })
+
+    it("does NOT allow users to subscribe to the email list", () => {
+        cy.getByData("email-input").type("tom")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+    })
+
+    it("does NOT allow users to subscribe with already subscribed emails", () => {
+        cy.getByData("email-input").type("john@example.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("server-error-message").should("exist").contains('Please use a different email address.')
+    })
+  })
+  
