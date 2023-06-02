d4d48318dfa953e2f323ed22c0f3098b8118b8fd
gabriel.torres <gabriel.torres@dynamox.net>
2023-06-02 11:27:09 -0300

    test: add checkoutCart w/mocked dispatch tests

--- Project/redux-shopping-cart-testing/src/features/cart/cartSlice.test.ts
+++ Project/redux-shopping-cart-testing/src/features/cart/cartSlice.test.ts
@@ -5,7 +5,8 @@ import cartReducer, {
     removeFromCart,
     getNumItems,
     getMemoizedNumItems,
-    getTotalPrice,  
+    getTotalPrice,
+    checkoutCart,
   } from "./cartSlice";
   import type { RootState } from "../../app/store";
   import products from "../../../public/products.json";
@@ -24,10 +25,49 @@ import cartReducer, {
       }
     }
   })
+
   
-  test("checkout should work", async () => {
-    await api.checkout({ fakeItem: 4 });
+  describe("thunks", () => {
+    describe("checkoutCart w/mocked dispatch", () => {
+      it("should checkout", async () => {
+        const dispatch = jest.fn();
+        const state: RootState = {
+          products: { products: {} },
+          cart: { checkoutState: "READY", errorMessage: "", items: { abc: 123 } },
+        };
+        const thunk = checkoutCart();
+        await thunk(dispatch, () => state, undefined);
+
+        // console.log(dispatch.mock.calls)
+
+        const { calls } = dispatch.mock;
+        expect(calls).toHaveLength(2);
+        expect(calls[0][0].type).toEqual("cart/checkout/pending");
+        expect(calls[1][0].type).toEqual("cart/checkout/fulfilled");
+        expect(calls[1][0].payload).toEqual({ success: true });
+      });
+      it("should fail with no items", async () => {
+        const dispatch = jest.fn();
+        const state: RootState = {
+          products: { products: {} },
+          cart: { checkoutState: "READY", errorMessage: "", items: {} },
+        };
+        const thunk = checkoutCart();
+        
+        await thunk(dispatch, () => state, undefined);
+        const { calls } = dispatch.mock;
+
+        console.log(dispatch.mock.calls)
+
+        expect(calls).toHaveLength(2);
+        expect(calls[0][0].type).toEqual("cart/checkout/pending");
+        expect(calls[1][0].type).toEqual("cart/checkout/rejected");
+        expect(calls[1][0].payload).toEqual(undefined);
+        expect(calls[1][0].error.message).toEqual('Must include cart items');
+      });
+    });
   });
+
   describe("cart reducer", () => {
     test("an empty action", () => {
       const initialState = undefined;
