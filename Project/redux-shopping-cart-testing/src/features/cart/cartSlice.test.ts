19be905764dd139e3902d2179b2463cba665ad26
gabriel.torres <gabriel.torres@dynamox.net>
2023-06-02 11:55:52 -0300

    test: add more test cases to checkoutCart w/mocked redux store

--- Project/redux-shopping-cart-testing/src/features/cart/cartSlice.test.ts
+++ Project/redux-shopping-cart-testing/src/features/cart/cartSlice.test.ts
@@ -25,6 +25,7 @@ import cartReducer, {
       async checkout(items: api.CartItems = {}) {
         const empty = Object.keys(items).length === 0;
         if (empty) throw new Error("Must include cart items");
+        if (items.evilItem > 0) throw new Error();
         if (items.badItem > 0) return { success: false };
         return { success: true };
       }
@@ -93,6 +94,7 @@ import cartReducer, {
         expect(actions[1].error.message).toEqual('Must include cart items');
       });
 
+      //Chapter 14
     describe("checkoutCart w/full redux store", () => {
       it("should checkout with items", async () => {
         const state = getStateWithItems({ testItem: 3 });
@@ -114,6 +116,40 @@ import cartReducer, {
           errorMessage: "Must include cart items"
         })
       });
+
+      //Chapter 15
+
+      it("should handle an error response", async () => {
+        const state = getStateWithItems({ badItem: 7 });
+        const store = getStoreWithState(state);
+        await store.dispatch(checkoutCart());
+        expect(store.getState().cart).toEqual({
+          items: { badItem: 7 },
+          checkoutState: "ERROR",
+          errorMessage: ""
+        });
+      });
+
+      it("should handle an empty error message", async () => {
+        const state = getStateWithItems({ evilItem: 7 });
+        const store = getStoreWithState(state);
+        await store.dispatch(checkoutCart());
+        expect(store.getState().cart).toEqual({
+          items: { evilItem: 7 },
+          checkoutState: "ERROR",
+          errorMessage: ""
+        });
+      });
+      it("should be pending before checking out", async () => {
+        const state = getStateWithItems({ goodItem: 7 });
+        const store = getStoreWithState(state);
+        expect(store.getState().cart.checkoutState).toEqual("READY");
+        const action = store.dispatch(checkoutCart());
+        expect(store.getState().cart.checkoutState).toEqual("LOADING");
+        await action;
+        expect(store.getState().cart.checkoutState).toEqual("READY");
+      });
+
     });
   });
 
