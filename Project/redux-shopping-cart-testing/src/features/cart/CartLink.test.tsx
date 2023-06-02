4c4afc3f4cd5fe9089c060b4b1f28a2bb871e40f
gabriel.torres <gabriel.torres@dynamox.net>
2023-06-02 12:56:46 -0300

    test: add more test cases to CartLink.test

---
+++ Project/redux-shopping-cart-testing/src/features/cart/CartLink.test.tsx
@@ -0,0 +1,42 @@
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { CartLink } from "./CartLink";
+import { store } from "../../app/store";
+import { addToCart, updateQuantity, removeFromCart } from "./cartSlice";
+
+test("should contain a link", () => {
+  renderWithContext(<CartLink />);
+  expect(screen.getByRole("link")).toBeInTheDocument();
+});
+
+test("should show text when there are no items", () => {
+    renderWithContext(<CartLink />);
+    const link = screen.getByRole("link");
+    expect(link).toHaveTextContent("Cart");
+    expect(link).not.toHaveTextContent("0");
+    expect(link).not.toHaveTextContent("1");
+  });
+  
+  test("should show the correct number of items", () => {
+    store.dispatch(addToCart("testItem"));
+    renderWithContext(<CartLink />);
+    const link = screen.getByRole("link");
+    expect(link).toHaveTextContent("1");
+    store.dispatch(updateQuantity({ id: "testItem", quantity: 5 }));
+    expect(link).toHaveTextContent("5");
+    store.dispatch(addToCart("anotherItem"));
+    expect(link).toHaveTextContent("6");
+    store.dispatch(removeFromCart("testItem"));
+    store.dispatch(removeFromCart("anotherItem"));
+    expect(link).toHaveTextContent("Cart");
+  });
+
+function renderWithContext(element: React.ReactElement) {
+  render(
+    <Provider store={store}>
+      <Router>{element}</Router>
+    </Provider>
+  );
+}
