a4c27b286185c8655634b14f486035f208b1e5b0
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-06-15 19:53:23 -0300

    Merge branch 'origin/course-scale-react-dev-nx'

--- projects/scale-react-dev-with-nx/libs/store-application/feature-game-details/README.md
+++
@@ -1,7 +0,0 @@
-# store-application-feature-game-details
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test store-application-feature-game-details` to execute the unit tests via [Vitest](https://vitest.dev/).
--- projects/scale-react-dev-with-nx/libs/store-application/util-formatters/src/lib/store-application-util-formatters.spec.ts
+++
@@ -1,9 +0,0 @@
-import { storeApplicationUtilFormatters } from './store-application-util-formatters';
-
-describe('storeApplicationUtilFormatters', () => {
-  it('should work', () => {
-    expect(storeApplicationUtilFormatters()).toEqual(
-      'store-application-util-formatters'
-    );
-  });
-});
