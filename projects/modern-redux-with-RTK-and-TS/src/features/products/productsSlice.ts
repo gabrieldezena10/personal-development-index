e7847461bd136c64642ea38cc56c7f52e1096991
gabriel.torres <gabriel.torres@dynamox.net>
2023-06-28 09:23:25 -0300

    feat: create cartSlice, productsSlice and configureStore

---
+++ projects/modern-redux-with-RTK-and-TS/src/features/products/productsSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { Product } from "../../app/api";
+
+export interface ProductState {
+    products: {
+        [id: string]: Product
+    }
+}
+const initialState: ProductState = {
+    products: {}
+}
+
+const productsSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {}
+
+});
+
+export default productsSlice.reducer;
